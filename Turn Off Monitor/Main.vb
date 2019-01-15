b12e4d4411025834b5eaa26e1636905d48b49da3
MinorMole <FansubTH.001@gmail.com>
2019-01-16 06:05:03 +0700

    First Release

---
+++ Turn Off Monitor/Main.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Main
+
+    <DllImport("user32.dll")>
+    Private Shared Function SendMessage(hWnd As Integer, hMsg As Integer, wParam As Integer, lParam As Integer) As Integer
+    End Function
+
+    Protected Overrides Sub SetVisibleCore(ByVal value As Boolean)
+        If Not IsHandleCreated Then
+            CreateHandle()
+            value = False
+            Threading.Thread.Sleep(1000)
+            SendMessage(Handle.ToInt32(), &H112, &HF170, 2)
+        End If
+        MyBase.SetVisibleCore(value)
+        Application.Exit()
+    End Sub
+
+End Class
---
+++ Turn Off Monitor/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Turn Off Monitor")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Turn Off Monitor")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1414d11a-5fea-4ad5-b9e2-8d39561b7e2e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
